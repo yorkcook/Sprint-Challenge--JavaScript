e2a6b99752b146fe367b3343b2d4d2b5b40a8b7f
York Cook <yorkcook@Johnny-Castle.local>
2019-05-10 11:43:36 -0400

    WIP: top of object arrays completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,52 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const joe = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: 7000 + 'kg',
+  length: 12 + 'm',
+  period: 'Late Cretaceous',
+};
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const bob = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: 2000 + 'kg',
+  length: 9 + 'm',
+  period: 'Late Jurassic',
+};
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const tom = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: 15 + 'kg',
+  length: 1.8 + 'm',
+  period: 'Late Cretaceous',
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(joe.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(tom.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(bob.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(joe.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+joe.sound = () => 'RAWERSRARARWERSARARARRRR!'
+console.log(joe.sound());
 
 
 // ==== Arrays ====
